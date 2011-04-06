8221877773359ae87897079d68af3f3d03a82111
Steven W <stevenwisniewski@gmail.com>
2011-04-05 21:31:14 -0400

    Added lifetime helpers and regen id option

--- sess.class.php
+++ sess.class.php
@@ -1,43 +1,60 @@
 <?PHP
 #	Simple Easy & Secure Sessions
 #	The goal of this class is to make PHP's session functions easier & safer to use.
+#	Written for PHP 5.2
 #
 #	Steven W
-#	03/16/2011
+#	4/5/2011
 #	https://github.com/stevenw/PHP-Simple-Sessions
 Class Sess
 {
 	public $sess_name = 'session';
-	public $lifetime = 3600;	// Default lifetime is One Hour
+	public $lifetime = 3600;	// Default lifetime is one hour
 	public $path = '/';
 	public $domain = '';
 	public $secure_only = FALSE;
 	public $http_only = TRUE;
+	public $regen_delete = FALSE;
 
-	function start_session()
+	public function start_session()
 	{
 		session_name($this->sess_name);
 		session_set_cookie_params($this->lifetime, $this->path, $this->domain, $this->secure_only, $this->http_only);
 		session_start();
+		session_regenerate_id($this->regen_delete);
 	}
 
-	function set_session($info = Array())
+	public function set_session($array = Array())
 	{
-		session_regenerate_id(TRUE);
-
-		foreach ($info as $k => $v)
+		foreach ($array as $k => $v)
 		{
 			$_SESSION[$k] = $v;
 		}
 	}
 
-	function kill_session()
+	public function kill_session()
 	{
 		$_SESSION = Array();
 		session_destroy();
 		setcookie($this->sess_name, "", time() - $this->lifetime);
 	}
+
+	// Helper Methods
+	public function lifetime_minutes($int = 1)
+	{
+		$this->lifetime = 60 * $int;
+	}
+
+	public function lifetime_hours($int = 1)
+	{
+		$this->lifetime = 3600 * $int;
+	}
+
+	public function lifetime_days($int = 1)
+	{
+		$this->lifetime = 86400 * $int;
+	}
 }
 
 /* End of file sess,class.php */
-/* Location: /sess,class.php */
+/* Location: /sess.class.php */
